65153ed776c42fa7b6bf29504395234e33fbb133
Daniela-HRF <146094242+Daniela-HRF@users.noreply.github.com>
2024-01-21 11:52:36 +0100

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -1,23 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     const tabs = document.querySelectorAll('.tab');
-
     tabs.forEach(function (tab) {
         tab.addEventListener('click', function () {
-            
             tabs.forEach(function (tab) {
                 tab.classList.remove('tab-active');
             });
-
             const contents = document.querySelectorAll('.content');
             contents.forEach(function (content) {
                 content.classList.remove('active');
             });
-
             this.classList.add('tab-active');
-
-            if (this.classList.contains('tab-nomDeLaxe')) {
-                const axisClass = this.classList[1];
-                const activeContent = document.querySelector(`.content.${axisClass}`);
+            if (this.classList.contains('tab-nomDePersonnage')) {
+                const personnageClass = this.classList[1];
+                const activeContent = document.querySelector(`.content.${personnageClass}`);
                 activeContent.classList.add('active');
             }
         });
